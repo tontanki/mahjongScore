4a90d52b14c7a5484e1c9e9dff090bdb351cb9f9
tontanki <dragonnight612@gmail.com>
2024-07-18 16:15:26 +0900

    add : timestampの漏れ

--- score/update.py
+++ score/update.py
@@ -1,7 +1,7 @@
 from player.player_data import PlayerData
 
 
-async def update_player_scores(player, point):
+async def update_player_scores(player, point, timestamp):
     try:
         player_data = PlayerData()
         player_data.update_data(player, point, timestamp)
