6b6a298bda67563037fcaa5848a03cf568dcfe95
tontanki <dragonnight612@gmail.com>
2024-07-19 13:42:18 +0900

    clean : calculatorのリファクタリング

--- score/calculator.py
+++ score/calculator.py
@@ -1,33 +1,25 @@
-from score.update import update_player_scores
-import datetime
-
-
 async def calculate_player_count(args):
     return len(args) // 2
 
 
 async def calculate_scores(args, player_count):
     base_score = 30000 if player_count == 4 else 40000
-    most_points = max([int(args[i + 1]) for i in range(0, len(args), 2)])
-    most_points_player = None
+    player_points = [(args[i], int(args[i + 1]))
+                     for i in range(0, len(args), 2)]
+    most_points = max(points for _, points in player_points)
+    most_points_player = next(
+        player for player, points in player_points if points == most_points)
     sum_points = 0
     scores = []
 
-    for i in range(0, len(args), 2):
-        player = args[i]
-        points = int(args[i + 1])
-
+    for player, points in player_points:
         if points == most_points:
-            if not most_points_player:
-                most_points_player = player
             continue
 
         points = round(points / 100) * 100
-        points = points - base_score
-        points = round(points / 1000)
+        points = round((points - base_score) / 1000)
         sum_points += points
-
         scores.append((player, points))
 
-    scores.append((most_points_player, abs(sum_points)))
+    scores.append((most_points_player, -sum_points))
     return scores
--- score/update.py
+++ score/update.py
@@ -8,15 +8,10 @@ async def update_player_scores(scores):
         history = HistoryData()
         player_data = PlayerData()
         timestamp = datetime.timestamp(datetime.now())
-        print(scores)
         for player, score in scores:
-            print(player, score)
             await history.insert_score(player, score, timestamp)
-            print(f"{player}のスコアを更新しました")
             total_score = await history.get_total_score(player)
-            print(f"{player}の合計スコアは{total_score}です")
             await player_data.update_score(player, total_score)
-            print(f"{player}のスコアを更新しました")
 
     except Exception as e:
         return f"エラーが発生しました: {e}"
