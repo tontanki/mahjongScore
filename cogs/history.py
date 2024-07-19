0d5ce653018e3bc1ab873057cc5a1b152442a172
tontanki <dragonnight612@gmail.com>
2024-07-19 11:15:42 +0900

    add : historyの表示形式を変更した

--- cogs/history.py
+++ cogs/history.py
@@ -1,19 +1,28 @@
 from discord.ext import commands
 from player.history_data import HistoryData
+from tabulate import tabulate
+from datetime import datetime
 
 
 class HistoryCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.history = HistoryData()
 
     @commands.command(name='history')
     async def history_command(self, ctx):
         """
         スコアの履歴を見る
         """
-        history = HistoryData()
-        result = history.show_history()
-        await ctx.send(result)
+        response = self.history.show_history()
+
+        data = [(player, datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), score)
+                for player, timestamp, score in response]
+
+        history_message = tabulate(
+            data, headers=["Player", "Timestamp", "Score"], tablefmt="grid")
+
+        await ctx.send(f"```\n{history_message}\n```")
 
 
 async def setup(bot):
--- score/update.py
+++ score/update.py
@@ -5,15 +5,12 @@ from player.player_data import PlayerData
 
 async def update_player_scores(scores):
     try:
-        print(f"Scores: {scores}")
         history = HistoryData()
         player_data = PlayerData()
         timestamp = datetime.timestamp(datetime.now())
         for player, score in scores:
             await history.insert_score(player, score, timestamp)
             total_score = await history.get_total_score(player)
-            print(f"Player: {player}, Score: {
-                score}, Total Score: {total_score}")
             await player_data.update_score(player, total_score)
 
     except Exception as e:
